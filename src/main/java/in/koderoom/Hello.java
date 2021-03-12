c6a3332038f6cc31b4695d963b0eca8f985ace85
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-12 20:08:12 +0530

    iniital commit.

---
+++ src/main/java/in/koderoom/Hello.java
@@ -0,0 +1,11 @@
+package in.koderoom;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+
+    public static String sayHello() {
+        return "Helloooo Woorlddd!";
+    }
+}
